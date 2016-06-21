58e5381d464e26a8bff94abe537968d3b47338fe
Vladimir Grechka <vladimir.grechka@gmail.com>
2016-06-21 19:53:56 +0300

    Stuff

--- aps/src/client-stuff.ts
+++ aps/src/client-stuff.ts
@@ -6,17 +6,36 @@
 
 const DEBUG_SIMULATE_SLOW_NETWORK = true
 
+require('regenerator-runtime/runtime')
 import static 'into-u/utils-client into-u/ui'
 
+let lang
+
 asn(global, {
-    initCustomerUI({lang, pageName}) {
+    initCustomerUI(opts) {
+        lang = opts.lang
         timeoutSet(DEBUG_SIMULATE_SLOW_NETWORK ? 1000 : 0, _=> {
             $('#wholePageSpinner').hide()
-            if (pageName === 'sign-in') {
-                ReactDOM.render(span('hi there'), document.getElementById('root'))
+            if (opts.pageName === 'sign-in') {
+                const emailInput = Input()
+                const passwordInput = Input({type: 'password'})
+                ReactDOM.render(
+                    form(
+                        diva({className: 'form-group'},
+                            label(t({en: 'E-mail', ua: 'Почта'})),
+                            emailInput),
+                        diva({className: 'form-group'},
+                            label(t({en: 'Password', ua: 'Пароль'})),
+                            passwordInput),
+                    ),
+                    byid0('root'))
             }
         })
     }
 })
+
+function t(ss) {
+    return ss[lang]
+}
 
 clog('Client code is kind of loaded')
