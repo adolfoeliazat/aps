d0d444498a80b75885699623f62b61adc94f2697
Vladimir Grechka <vladimir.grechka@gmail.com>
2016-06-22 13:55:43 +0300

    Stuff

---
+++ aps/sql/aps.sql
@@ -0,0 +1,11 @@
+﻿create table test_collation(id bigserial, name text);
+insert into test_collation(name) values;
+    ('ввв'),
+    ('ааа'),
+    ('ббб'),
+    ('ВВВ'),
+    ('ААА'),
+    ('БББ');
+select * from test_collation order by name desc;
+
+
