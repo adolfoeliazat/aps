f2c99d0afa918974066ab30c637bc90b50e73934
Vladimir Grechka <vladimir.grechka@gmail.com>
2016-06-26 18:14:02 +0300

    Stuff

--- aps/scrap/scrap-backend.ts
+++ aps/scrap/scrap-backend.ts
@@ -15,3 +15,31 @@ relog(uuid())
 pgQuery(`insert into users(email, hash, firstName, lastName) values($1, $2, $3, $4)`,
         ['foo', 'bar', 'baz', 'qux'])
 relog('ok')
+
+
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    host: '127.0.0.1',
+    port: 2525,
+    auth: {
+        user: 'into/mail',
+        pass: 'bigsecret'
+    }
+})
+var mailOptions = {
+    from: 'APS <noreply@aps.local>',
+    to: 'wilma.blue@test.shit',
+    subject: 'Hi there, honey',
+    html: '<b>Wat’s up, Wilma?</b><hr>And what’s down?'
+}
+relog(await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            return reject(error)
+            // return relog('ERROR', error)
+        }
+        resolve('Message sent: ' + info.response)
+    })
+}))
+
+
