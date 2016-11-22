218274c2d69f491890edf522c1537546e194bb7c
Vladimir Grechka <vladimir.grechka@gmail.com>
2016-11-22 12:25:39 +0200

    vovagrechka/aps#18

--- front/src/aps/front/test/TestSuite_Writer_Boot.kt
+++ front/src/aps/front/test/TestSuite_Writer_Boot.kt
@@ -6,8 +6,8 @@ class TestSuite_Writer_Boot : TestSuite {
     override val scenarios by lazy {listOf(
         TestWriter_Boot1(),
         TestWriter_Boot2(),
-        TestWriter_Boot3()
-//        TestWriter_Boot4()
+        TestWriter_Boot3(),
+        TestWriter_Boot4()
     )}
 }
 
