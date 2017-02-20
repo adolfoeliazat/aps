904e70154b865b409ef1d175a86518bf38c17de2
Vladimir Grechka <vladimir.grechka@gmail.com>
2017-02-20 09:31:00 +0200

    A piece of... code

--- front/src/aps/front/test/Test_UA_CrazyLong_2.kt
+++ front/src/aps/front/test/Test_UA_CrazyLong_2.kt
@@ -24,8 +24,8 @@ class Test_UA_CrazyLong_2 : FuckingScenario() {
     }
 
     val filesShortcutMode1 = FilesShortcutMode.B
-//    val startPoint = 1
-    val startPoint = 6
+    val startPoint = 1
+//    val startPoint = 6
     var currentPoint = 0
 
     override suspend fun run1() {
@@ -34,7 +34,7 @@ class Test_UA_CrazyLong_2 : FuckingScenario() {
             bannerHorizontalPosition = HorizontalPosition.RIGHT)
 
         forceFast()
-        // TestGlobal.skipAllFreakingAssertions = true
+        TestGlobal.skipAllFreakingAssertions = true
         initialTestShit(this)
 
         definePoint(1) {
